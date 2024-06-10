a842e6f70c2da81a93b4a4c0737d2c0a39c7ce70
Sanya Moses Adeniyi <54499732+akpcode@users.noreply.github.com>
2024-06-10 17:57:28 +0100

    toUCH

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 // import Header from './shared/header';//absolute import using jsconfig.js
 // import Footer from './shared/footer';
 import  LandingPage  from "../src/views/landingPage";
-import { ContactPage } from 'views';
+// import { ContactPage } from 'views';
 
 function App() {
   return (
